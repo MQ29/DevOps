724abfe67abdbf43562d9299b0cd7330c0be27a7
Mateusz Łukaszewicz <mateusz.lukaszewicz@microsoft.wsei.edu.pl>
2024-11-15 12:39:57 +0100

    d

--- Apka/seed.sql
+++
@@ -1,4 +0,0 @@
-USE mydb;
-
-INSERT INTO Users (FirstName, LastName, Email) VALUES ('John', 'Doe', 'john.doe@example.com');
-INSERT INTO Users (FirstName, LastName, Email) VALUES ('Jane', 'Smith', 'jane.smith@example.com');
